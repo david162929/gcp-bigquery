849b6927705f75dcc90aa29cf53c3ef3608937d9
David Chen <45975185+david162929@users.noreply.github.com>
2019-06-27 18:54:14 +0800

    add eslint

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("OK");
+});
+
 app.listen(3000, () => {
     console.log("running on port 3000.");
 });
